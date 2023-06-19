1a5006632b0bdfab7859347fb0ea0aea77c47866
끼얏호우끼얏호우 <jiwooson.0816@gmail.com>
2023-06-19 10:11:42 +0900

    Initial Commit

---
+++ Op/OpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpApp.swift
+//  Op
+//
+//  Created by sonjiwoo on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct OpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
